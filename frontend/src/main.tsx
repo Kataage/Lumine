14c9f8a62c2e47a7fd5bf0179fcb02246c28ea09
Kataage <50143135+Kataage@users.noreply.github.com>
2026-09-18 04:36:06 +0900

    feat: mount Lumine dialog provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import "./detailPanel.css";
+import { AppDialogProvider } from "./components/AppDialogProvider";
 import { initializeMemoryImageCacheBudget } from "./utils/imageCacheSettings";
 
 async function start() {
   await initializeMemoryImageCacheBudget();
   ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
-      <App />
+      <AppDialogProvider>
+        <App />
+      </AppDialogProvider>
     </React.StrictMode>
   );
 }
